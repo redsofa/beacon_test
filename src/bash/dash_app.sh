69c141a5113d22741a60ed8f1e2ec40953eecd5a
redsofa <rene@redsofa.ca>
2024-02-15 14:39:54 -0400

    checkpoint

---
+++ src/bash/dash_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Command to start the docker container 
+sudo docker-compose \
+	--project-directory ../../docker \
+	run \
+	-p 8080:8080 \
+	--name dash_app \
+	--rm dash_app \
+	hupper -m dash_app.app
+
---
+++ src/python/modules/aip/utils.py
@@ -0,0 +1,2 @@
+def ret_test():
+    return 'title'
